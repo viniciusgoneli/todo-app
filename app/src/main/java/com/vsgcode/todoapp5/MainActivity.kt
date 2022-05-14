4e952d1157600082015dbfc214beac0ac560f4fa
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-05-14 10:04:06 -0300

    Set listFragment layout.

--- app/src/main/java/com/vsgcode/todoapp5/MainActivity.kt
+++ app/src/main/java/com/vsgcode/todoapp5/MainActivity.kt
@@ -18,11 +18,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
