cc47d8a764647d054689fcded025c56d1542f6cd
Vinicius Goneli <viniciusgoneli_2012@hotmail.com>
2022-05-14 14:12:21 -0300

    Created TaskDAO interface.

---
+++ app/src/main/java/com/vsgcode/todoapp5/model/TaskDAO.kt
@@ -0,0 +1,17 @@
+package com.vsgcode.todoapp5.model
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface TaskDAO {
+
+    @Query("SELECT * FROM task_table ORDER BY id ASC")
+    suspend fun getAllTasks() : LiveData<List<Task>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTask(task : Task)
+}
